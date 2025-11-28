6696b9ed7a5b7708d109a8b55ee8cf3dbba851d7
Vadim Gedz <github@shini4i.dev>
2025-11-28 16:22:19 +0200

    chore: add flake.nix setup

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Local environment for disposable-k8s-cluster";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = lib.systems.flakeExposed;
+    in {
+      devShells = lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go-task
+              jsonnet
+              kubectl
+              tfswitch
+              terraform-docs
+              tflint
+              trivy
+              trufflehog
+              pre-commit
+              checkov
+            ];
+
+            shellHook = ''
+              export KUBECONFIG="$PWD/kubeconfig"
+            '';
+          };
+        });
+    };
+}
